82e6369dafec8563091359f43c7f91af104cfaeb
igiguray <38431264+igiguray@users.noreply.github.com>
2023-06-08 00:06:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '茜茜宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooi7r6ZOoHq2aVQT8qzfKfAV0VII',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TQYWT7sb1CkBYTVOfHcO88fpPOSuoUeJBVAnfJCwpdc',
+      useTemplateId: 'Y05YXBzHxtcjPPrGm743tMVo0-Uq2yR0jT7t7LjEa1I',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-17',
       festivals: [
