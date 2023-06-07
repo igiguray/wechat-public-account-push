7b337e8b6fc784c96c1976c459e556d72f994b50
igiguray <38431264+igiguray@users.noreply.github.com>
2023-06-08 00:38:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,46 +1,110 @@
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
-    APP_ID: "wxf73816cfe8d1b052",
-    APP_SECRET: "09c79cb59fcefa17aa11c3241cfce0a1",
-    IS_SHOW_COLOR: true,
-    CALLBACK_TEMPLATE_ID: "humiOa0DDFUGNJyHhPBhxY14ZSJL-It2rstoZ3BKo6k",
-    CALLBACK_USERS: [
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wxf73816cfe8d1b052',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: '09c79cb59fcefa17aa11c3241cfce0a1',
+
+  PROVINCE: '湖北',
+  CITY: '武汉',
+
+  IS_SHOW_COLOR: true,
+
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: false,
+
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: false,
+
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
+
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: false,
+
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '茜茜宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ooi7r6ZOoHq2aVQT8qzfKfAV0VII',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'Q4FQ8b2ExW1_riHykmb6TUHMomSoHJ0u65crsSik2CA',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '11-17',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        //{
+        //  type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        //},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-            "name": "自己",
-            "id": "ooi7r6ZOoHq2aVQT8qzfKfAV0VII"
-        }
-    ],
-    USERS: [
+          type: '生日', name: '宝贝', year: '1997', date: '11-17',
+        },
         {
-            "name": "茜茜宝贝",
-            "id": "ooi7r6ZOoHq2aVQT8qzfKfAV0VII",
-            "useTemplateId": "Q4FQ8b2ExW1_riHykmb6TUHMomSoHJ0u65crsSik2CA",
-            "province": "湖北",
-            "city": "武汉",
-            "horoscopeDate": "11-17",
-            "horoscopeDateType": "今日",
-            "festivals": [
-                {
-                    "type": "生日",
-                    "name": "生日",
-                    "date": "11-17",
-                    "year": "1997"
-                }
-            ],
-            "customizedDateList": []
-        }
-    ],
-    SWITCH: {
-        "weather": true,
-        "holidaytts": true,
-        "CIBA": false,
-        "oneTalk": false,
-        "earthyLoveWords": true,
-        "momentCopyrighting": false,
-        "poisonChickenSoup": false,
-        "poetry": false,
-        "horoscope": false,
-        "birthdayMessage": true
+          type: '节日', name: '相识纪念日', year: '2023', date: '01-21',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-03-04' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: 'humiOa0DDFUGNJyHhPBhxY14ZSJL-It2rstoZ3BKo6k',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ooi7r6ZOoHq2aVQT8qzfKfAV0VII',
     }
+  ],
+
 }
 
 module.exports = USER_CONFIG
